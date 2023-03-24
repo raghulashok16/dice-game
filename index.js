bcfc5d059c6ba922bae208fe453648e6bedd0ee5
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-03-24 19:07:43 +0530

    toggle players added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 $(document).ready(function () {
-    let a = 0;
+    $('#p2btn').hide();
+    let p1totalScore = 0
+    let p1currentScore = 0;
+    let p2totalScore = 0
+    let p2currentScore = 0;
+    let p1temp = 0;
+    let p2temp = 0;
+
     $("#p1btn").click(function () {
-        a = a + 1;
-        $('#p1ts').text(`${a}`);
-        let temp = Math.floor(Math.random() * 6) + 1;
-        $("#diceImg").addClass("roll");
-        $('#diceImg').attr('src', `${temp}.png`);
+        p1temp = 0;
+        p1temp = Math.floor(Math.random() * 6) + 1;
+        $("#diceImg").toggleClass("roll");
+        $('#diceImg').attr('src', `${p1temp}.png`);
         $('#p1btn').hide();
+        const myTimeout = setTimeout(p1tgl, 800);
+        function p1tgl() {
+            $('#p1btn').show();
+            $("#diceImg").toggleClass("roll");
+            if (p1temp === 6) {
+                $('#p1btn').hide();
+                $('#p2btn').show();
+            }
+        }
+    });
+
+    $("#p2btn").click(function () {
+        p2temp = 0;
+        p2temp = Math.floor(Math.random() * 6) + 1;
+        $("#diceImg").toggleClass("roll");
+        $('#diceImg').attr('src', `${p2temp}.png`);
+        $('#p2btn').hide();
+        const myTimeout = setTimeout(p2tgl, 800);
+        function p2tgl() {
+            $('#p2btn').show();
+            $("#diceImg").toggleClass("roll");
+            if (p2temp === 6) {
+                $('#p2btn').hide();
+                $('#p1btn').show();
+            }
+        }
     });
 });  
