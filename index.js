8843fb5d573d693bfda3717f21fb7efc9d7aedf5
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-03-24 18:29:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+    let a = 0;
     $("#p1btn").click(function () {
-
+        a = a + 1;
+        $('#p1ts').text(`${a}`);
         let temp = Math.floor(Math.random() * 6) + 1;
         $("#diceImg").addClass("roll");
         $('#diceImg').attr('src', `${temp}.png`);
